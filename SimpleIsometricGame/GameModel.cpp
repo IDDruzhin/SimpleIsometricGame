6866c57e954978f9b98f7327964694ac0e051a1a
id.druzhin <id.druzhin@gmail.com>
2019-01-04 23:33:20 +0300

    GameModel added

---
+++ SimpleIsometricGame/GameModel.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "GameModel.h"
+
+
+GameModel::GameModel()
+{
+}
+
+
+GameModel::~GameModel()
+{
+}
---
+++ SimpleIsometricGame/GameModel.h
@@ -0,0 +1,8 @@
+#pragma once
+class GameModel
+{
+public:
+	GameModel();
+	~GameModel();
+};
+
