ed3076649c26b14bdb1e1cec7d48007df1ef1c4e
id.druzhin <id.druzhin@gmail.com>
2019-01-07 18:57:18 +0300

    Template Vector2D added

--- SimpleIsometricGame/Utils.h
+++ SimpleIsometricGame/Utils.h
@@ -5,6 +5,57 @@
 typedef unsigned char uchar;
 typedef unsigned int uint;
 
+template <class T>
+class Vector2D
+{
+public:
+	T x;
+	T y;
+	Vector2D() {};
+	Vector2D(T _x, T _y) :x(_x), y(_y) {};
+	Vector2D& operator=(const Vector2D &copy)
+	{
+		x = copy.x;
+		y = copy.y;
+		return *this;
+	}
+	Vector2D operator+(const Vector2D &right)
+	{
+		Vector2D res;
+		res.x = x + right.x;
+		res.y = y + right.y;
+		return res;
+	}
+	Vector2D operator-(const Vector2D &right)
+	{
+		Vector2D res;
+		res.x = x - right.x;
+		res.y = y - right.y;
+		return res;
+	}
+	T Dot(const Vector2D &v)
+	{
+		return (x*v.x + y * v.y);
+	};
+	T Norm()
+	{
+		return std::sqrt(Dot(*this));
+	};
+	T Dot(Vector2D v1, Vector2D v2)
+	{
+		return (v1.x*v2.x + v1.y*v2.y);
+	};
+	T Norm(Vector2D v)
+	{
+		return std::sqrt(Dot(v, v));
+	};
+};
+
+typedef Vector2D<float> float2;
+typedef Vector2D<int> int2;
+typedef Vector2D<uint> uint2;
+
+/*
 struct float2
 {
 	float x;
@@ -27,7 +78,6 @@ struct float2
 	{
 		return std::sqrt(Dot(v, v));
 	};
-	/*
 	float2(float _x, float _y) :x(_x), y(_y) {};
 	float2& operator=(const float2 &copy)
 	{
@@ -35,7 +85,6 @@ struct float2
 		y = copy.y;
 		return *this;
 	}
-	*/
 };
 
 struct int2
@@ -53,6 +102,7 @@ struct uint2
 	uint2() {};
 	uint2(uint _x, uint _y) :x(_x), y(_y) {};
 };
+*/
 
 struct Rect
 {
